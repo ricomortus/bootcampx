cafc620be562946f1a617834abb1049be508e498
Rico Chow <rico.ck.chow@gmail.com>
2023-11-29 17:07:59 -0500

    Add tables for teachers and assistance requests + dummy data.

--- 2_queries_joins/students_total_assignments_submission.sql
+++ 2_queries_joins/students_total_assignments_submission.sql
@@ -2,3 +2,11 @@ SELECT sum(assignment_submissions.duration) as total_duration
 FROM assignment_submissions
 JOIN students ON students.id = student_id
 WHERE students.name = 'Ibrahim Schimmel';
+
+
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHEN students.submissions <= 100
+GROUP BY students.name;
